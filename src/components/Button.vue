1a21a6e9e0ac26d6eb878c1e812ff1aef412a351
Ambre <ambre1709@yahoo.com>
2022-02-10 10:28:27 +0100

    props color + component

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    :class="[btn !== 'large' ? '' : 'col-span-2']"
+    class="relative cursor-pointer"
+  >
+    <div
+      :class="[
+        color !== 'secondary'
+          ? 'bg-yellow-500 dark:bg-purpletor-light text-white dark:text-yellowtor'
+          : 'bg-white dark:bg-bluetor text-black',
+      ]"
+      class="relative z-10 rounded-md h-14 flex items-center justify-center text-xl"
+    >
+      {{ label }}
+    </div>
+    <div
+      :class="[
+        color !== 'secondary'
+          ? 'bg-yellow-800 dark:bg-purpletor-lightest'
+          : 'bg-gray-300 dark:bg-bluetor-light',
+      ]"
+      class="absolute -bottom-1 right-0 rounded-md h-14 w-full"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {},
+    color: {},
+    btn: {},
+  },
+};
+</script>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,87 @@
+<template>
+  <h1
+    class="text-3xl font-bold my-20 text-center text-black dark:text-yellowtor"
+  >
+    Hello world!
+  </h1>
+  <div class="container flex justify-center">
+    <div
+      class="bg-teal-600 dark:bg-purpletor-darkest w-96 h-xl text-white font-bold p-8 rounded-md"
+    >
+      <h2 class="text-2xl text-white dark:text-yellowtor mb-4">calc</h2>
+      <div
+        class="bg-teal-800 dark:bg-purpletor-dark text-white dark:text-yellowtor mb-8 rounded-md text-right text-2xl pr-6 py-4"
+      >
+        <span class="text-base">{{ formule }}</span
+        ><br />
+        <span v-if="!error">{{ result }}</span
+        ><span v-if="error">{{ error }}</span>
+      </div>
+
+      <div class="bg-teal-800 dark:bg-purpletor-dark p-6 rounded-md">
+        <div class="grid grid-cols-4 gap-4">
+          <Button label="7" @click="operate(7)" />
+          <Button label="8" @click="operate(8)" />
+          <Button label="9" @click="operate(9)" />
+          <Button color="secondary" label="DEL" @click="drop()" />
+          <Button label="4" @click="operate(4)" />
+          <Button label="5" @click="operate(5)" />
+          <Button label="6" @click="operate(6)" />
+          <Button label="+" @click="operate('+')" />
+          <Button label="1" @click="operate(1)" />
+          <Button label="2" @click="operate(2)" />
+          <Button label="3" @click="operate(3)" />
+          <Button label="-" @click="operate('-')" />
+          <Button label="." @click="operate('.')" />
+          <Button label="0" @click="operate(0)" />
+          <Button label="/" @click="operate('/')" />
+          <Button label="x" @click="operate('*')" />
+          <Button
+            color="secondary"
+            btn="large"
+            label="RESET"
+            @click="cleanAll()"
+          />
+          <Button color="secondary" btn="large" label="=" @click="equal()" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      error: "",
+      formule: "",
+      result: 0,
+    };
+  },
+  methods: {
+    operate(element) {
+      this.formule += element;
+    },
+    equal() {
+      try {
+        this.result = eval(this.formule);
+      } catch (error) {
+        this.error = "ERROR";
+      }
+    },
+    drop() {
+      this.formule = this.formule.slice(0, -1);
+      this.error = "";
+    },
+    cleanAll() {
+      this.error = "";
+      this.formule = "";
+      this.result = 0;
+    },
+  },
+};
+</script>
