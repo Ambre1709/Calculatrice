f6c79f95e86901edebd101fa48a73a59bf23b16f
Ambre <ambre1709@yahoo.com>
2022-02-22 09:26:20 +0100

    ✨ affichage dimensions window

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <Darkmode />
-  <Calculator />
+  <div class="relative">
+    <Darkmode />
+    <Calculator />
+    <Width class="absolute top-2 right-3" />
+  </div>
 </template>
 
 <script>
 import Darkmode from "./components/Darkmode.vue";
 import Calculator from "./components/Calculator.vue";
+import Width from "./components/Width.vue";
 export default {
   components: {
     Darkmode,
     Calculator,
+    Width,
   },
 };
 </script>
---
+++ src/components/Width.vue
@@ -0,0 +1,32 @@
+<template>
+  <p class="text-right text-gray-700 dark:text-gray-300">
+    Width: {{ window.width }} px<br />
+    Height: {{ window.height }} px
+  </p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      window: {
+        width: 0,
+        height: 0,
+      },
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.window.width = window.innerWidth;
+      this.window.height = window.innerHeight;
+    },
+  },
+};
+</script>
